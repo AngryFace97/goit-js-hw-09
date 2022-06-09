7c0ede6d9757b1d3743930205590cd2bfc6b08f0
Paweł Kłodowski <angryface97@gmail.com>
2022-06-09 21:20:12 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,7 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
 
 const startBtn = document.querySelector('button[data-start]');
 const dateChosen = document.querySelector('#datetime-picker');
